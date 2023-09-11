6920c1f808b6870b3676e92c59d0c024bfc75ec4
Crowdbotics <team@crowdbotics.com>
2023-09-11 15:05:55 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "atriu-43332",
+  url: "https://atriu-43332.botics.co",
+  api: "https://atriu-43332.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
